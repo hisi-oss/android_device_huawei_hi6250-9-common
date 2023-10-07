20b854139a425fc5b21a7106f194bd6a998a94ff
me-cafebabe <me.cafebabe@gmail.com>
2023-10-07 17:26:01 +0800

    hi6250-9-commom: Switch to legacy WiFi HIDL HAL
    * With the new WiFi AIDL HAL, the framework thinks the device
      supports interface combination for some unknown reason,
      even though the service doesn't advertise it. As a result,
      the WiFi service fails on configureChip method.
    
    Change-Id: I0c3fa7b4c947c3525c30f7cef0b2674bee224da7

--- common.mk
+++ common.mk
@@ -292,7 +292,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     hostapd \
     wpa_supplicant \
     wpa_supplicant.conf
