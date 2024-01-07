804cc00bb62e7b6bdceee55115470b0a786a31f5
Tim Zimmermann <tim@linux4.de>
2024-01-07 21:39:15 +0100

    hi6250-9-common: Include recovery_vendor to OTA
    Change-Id: I0520a90a7a17e3ce1681482290beaa37733743b7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,6 +60,9 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Platform
 TARGET_BOARD_PLATFORM := hi6250
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := hardware/huawei/releasetools
+
 # VNDK
 PRODUCT_USE_VNDK_OVERRIDE := true
 
