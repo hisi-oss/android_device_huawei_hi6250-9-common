b2d9c450d91837aeb29c6cfb0cab96625ace9a64
R0rt1z2 <me@r0rt1z2.com>
2024-01-07 21:19:11 +0100

    hi6250-9-common: Set boot image flags
    Change-Id: I591715c21f281de13e43f2d9d388f2cfc17b48dc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,5 +18,15 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# Boot
+BOARD_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_HAS_RAMDISK := false
+BOARD_CUSTOM_BOOTIMG_MK := hardware/huawei/mkbootimg.mk
+
+BOARD_KERNEL_BASE := 0x00478000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := loglevel=4 coherent_pool=512K page_tracker=on slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x0ff88000 --second_offset 0x00e88000 --tags_offset 0x07988000
+
 # Inherit the proprietary files
 include vendor/huawei/hi6250-9-common/BoardConfigVendor.mk
