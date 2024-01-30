d21dcdbec1891ee66b91db4e1c8c569277c8d7ca
R0rt1z2 <me@r0rt1z2.com>
2024-01-30 17:14:48 +0100

    hi6250-9-common: Initial minimised rootdir
    Change-Id: I956e52acdb6e17e92f9924fc2c033a6e0d9b4840

--- common.mk
+++ common.mk
@@ -7,6 +7,17 @@
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.hi6250 \
+    fstab.modem \
+    init.hi6250.rc \
+    init.hi6250.usb.rc \
+    ueventd.hi6250.rc
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/etc/connectivity/,$(TARGET_COPY_OUT_VENDOR)/etc/init/connectivity)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
