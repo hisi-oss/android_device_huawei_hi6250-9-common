d7c7ca9e5fc449ac02f845d0f62276c3799f6b6b
R0rt1z2 <me@r0rt1z2.com>
2024-01-31 00:27:42 +0100

    hi6250-9-common: Configure Thermal stack

--- common.mk
+++ common.mk
@@ -152,6 +152,11 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
