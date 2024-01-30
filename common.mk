3499180e182cb90303a4ceef7d8b252ed461287e
R0rt1z2 <me@r0rt1z2.com>
2024-01-31 00:27:42 +0100

    hi6250-9-common: Configure Thermal stack

--- common.mk
+++ common.mk
@@ -121,6 +121,11 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/hisi \
     hardware/huawei
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
