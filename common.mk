83e7cc8487c22a8878335c9659f79a28a31d980d
R0rt1z2 <me@r0rt1z2.com>
2024-01-30 19:27:15 +0100

    hi6250-9-common: Configure Display stack

--- common.mk
+++ common.mk
@@ -7,6 +7,16 @@
 ## Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.2-service \
+    android.hardware.graphics.mapper@2.0-impl
+
+PRODUCT_PACKAGES += \
+    libion
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
