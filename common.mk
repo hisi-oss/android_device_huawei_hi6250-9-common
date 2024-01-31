3620bd5b5601f55d58e65ce9452c5faae5a5b419
R0rt1z2 <me@r0rt1z2.com>
2024-01-31 11:47:15 +0100

    hi6250-9-common: Configure RIL stack

--- common.mk
+++ common.mk
@@ -175,6 +175,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/power/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.0.vendor \
+    android.hardware.radio@1.1.vendor \
+    android.hardware.radio@1.2.vendor \
+    android.hardware.radio@1.6.vendor \
+    android.hardware.radio.config@1.3.vendor \
+    android.hardware.radio.deprecated@1.0.vendor
+
+PRODUCT_PACKAGES += \
+    librilutils
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.hi6250 \
