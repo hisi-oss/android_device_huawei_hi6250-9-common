fd4cae330f9c8e309e331471ad62dd7292deba1c
R0rt1z2 <me@r0rt1z2.com>
2024-01-31 11:47:15 +0100

    hi6250-9-common: Configure RIL stack

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -74,6 +74,10 @@ TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := hardware/hisi/releasetools
 
+# RIL
+BOARD_PROVIDES_LIBRIL := true
+ENABLE_VENDOR_RIL_SERVICE := true
+
 # Root
 BOARD_ROOT_EXTRA_FOLDERS += \
     3rdmodem \
