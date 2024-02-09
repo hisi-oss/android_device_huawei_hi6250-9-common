4553a53f01996a077e0b45b6c88008c569ef1677
Bruno Martins <bgcngm@gmail.com>
2024-02-09 12:22:03 +0100

    hi6250-9-common: Blacklist fingerprint HAL key events
    Change-Id: I57db108f5250c120726b77dd32f4666917c9eb7c

--- common.mk
+++ common.mk
@@ -105,6 +105,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/fingerprint.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/fingerprint.kl
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
