3301ff88d8fcb4fb3fb83c009ea5cca32a15c25e
Giovanni Ricca <giovanniricca@proton.me>
2024-03-19 22:51:10 +0100

    hi6250-9-common: Inherit `hardware/huawei`

--- common.mk
+++ common.mk
@@ -10,7 +10,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/hisi
+    hardware/hisi \
+    hardware/huawei
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/huawei/hi6250-9-common/hi6250-9-common-vendor.mk)
