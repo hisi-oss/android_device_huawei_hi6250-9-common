76ab75e5e5ab92bfb7e2b3d7d25f983c73b8f6b6
R0rt1z2 <me@r0rt1z2.com>
2024-03-20 13:02:50 +0100

    hi6250-9-common: Start rild when ril props are ready
    Co-authored-by: A2L5E0X1 <alexed25@protonmail.com>
    Change-Id: I8b40ac39778336dea7f5bdfc0ad7ab6ba1830b58

--- extract-files.sh
+++ extract-files.sh
@@ -63,6 +63,10 @@ function blob_fixup() {
         vendor/etc/init/android.hardware.drm@1.1-service.widevine.rc)
             sed -i 's/preavs/vendor/g' "${2}"
             ;;
+        vendor/etc/init/rild.rc)
+            sed -i '1i on property:sys.rilprops_ready=1\n    start vendor.ril-daemon\n' "${2}"
+            echo "    disabled" >> "${2}"
+            ;;
         vendor/lib64/hw/audio.primary_hisi.hi6250.so)
             "${PATCHELF}" --add-needed "libprocessgroup.so" "${2}"
             ;;
